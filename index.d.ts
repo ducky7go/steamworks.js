9aaa1a7ce0f38b9701813f618f4c0ed768cbc186
newbe36524 <newbe36524@qq.com>
2025-12-29 09:56:41 +0800

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
-export function init(appId?: number): Omit<Client, "init" | "runCallbacks">;
+// Re-export all types from client.d.ts
+export type * from './client.d';
+
+export function init(appId?: number): Omit<typeof import('./client.d'), 'init' | 'runCallbacks'>;
 export function restartAppIfNecessary(appId: number): boolean;
 export function electronEnableSteamOverlay(disableEachFrameInvalidation?: boolean): void;
-export type Client = typeof import("./client.d");
-export const SteamCallback: typeof import("./client.d").callback.SteamCallback;
+export const SteamCallback: typeof import('./client.d').callback.SteamCallback;
