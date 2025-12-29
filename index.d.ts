3b932c865ded5687a80acc539a14b4ff870905bf
newbe36524 <newbe36524@qq.com>
2025-12-29 16:26:35 +0800

    1

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
+// Re-export all types from client.d.ts
+export type * from './client.d';
 export function init(appId?: number): Omit<Client, "init" | "runCallbacks" | "shutdown">;
 export function shutdown(): void;
+export function isConnected(): boolean;
 export function restartAppIfNecessary(appId: number): boolean;
 export function electronEnableSteamOverlay(disableEachFrameInvalidation?: boolean): void;
 export type Client = typeof import("./client.d");
